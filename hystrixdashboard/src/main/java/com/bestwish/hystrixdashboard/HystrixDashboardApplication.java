193d020b2da1a8ddf0ed8adccb66dcd4a445ce77
liu.junjie <1965483733@qq.com>
2018-03-21 22:26:13 +0800

    ok commit

--- hystrixdashboard/src/main/java/com/bestwish/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrixdashboard/src/main/java/com/bestwish/hystrixdashboard/HystrixDashboardApplication.java
@@ -7,13 +7,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableHystrix
 @EnableAutoConfiguration
 @EnableHystrixDashboard
-@EnableFeignClients
 public class HystrixDashboardApplication {
 
     public static void main(String[] args) {
