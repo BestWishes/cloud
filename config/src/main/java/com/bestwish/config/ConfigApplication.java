892ecd563bcbb1fb59db348b65a42c53d8e43ed5
liu.junjie <1965483733@qq.com>
2018-03-28 11:11:12 +0800

    启动类注解去除多余的部分

--- config/src/main/java/com/bestwish/config/ConfigApplication.java
+++ config/src/main/java/com/bestwish/config/ConfigApplication.java
@@ -5,14 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
 @EnableAutoConfiguration
-@EnableHystrix
 public class ConfigApplication {
 
     public static void main(String[] args) {
